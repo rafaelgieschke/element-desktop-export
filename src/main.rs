93efcb1f9f03fd062311148f68e7bc47e42348ff
Rafael Gieschke <rafael@gieschke.de>
2025-12-31 23:59:14 +0100

    feat: support Flatpak version of Element Desktop

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,55 @@
 use std::collections::HashMap;
 use std::{env, str};
 
+use aes::cipher::block_padding::Pkcs7;
+use aes::cipher::{BlockDecryptMut, KeyIvInit};
+use base64::prelude::*;
 use clap::Parser;
+
+fn find_element_dir() -> Option<std::path::PathBuf> {
+    [
+        env::home_dir()?.join("snap/element-desktop/current/.config/Element/"),
+        env::home_dir()?.join(".var/app/im.riot.Riot/config/Element/"),
+    ]
+    .into_iter()
+    .find(|path| path.exists())
+}
 
 #[derive(Parser)]
 struct Cli {
-    #[arg(default_value = env::home_dir().unwrap().join("snap/element-desktop/current/.local/share/keyrings/default.keyring").into_os_string())]
-    keyring: std::path::PathBuf,
-    #[arg(default_value = env::home_dir().unwrap().join("snap/element-desktop/current/.config/Element/EventStore/events.db").into_os_string())]
-    events_db: std::path::PathBuf,
+    #[arg(default_value = find_element_dir().unwrap_or("".into()).into_os_string())]
+    element_dir: std::path::PathBuf,
+    #[arg(short, long)]
+    keyring: Option<std::path::PathBuf>,
+    #[arg(short, long)]
+    events_db: Option<std::path::PathBuf>,
 }
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Cli::parse();
+async fn get_secret_basic_text(
+    config: serde_json::Value,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let mut secret = Vec::new();
+    for (key, val) in config["safeStorage"].as_object().ok_or("")? {
+        if key.starts_with("seshat|")
+            && let Some(val2) = val.as_str()
+        {
+            secret = BASE64_STANDARD.decode(val2)?;
+        }
+    }
 
+    // https://github.com/chromium/chromium/blob/main/components/os_crypt/sync/os_crypt_posix.cc
+    Ok(String::from_utf8(
+        cbc::Decryptor::<aes::Aes128>::new(
+            b"\xfd\x62\x1f\xe5\xa2\xb4\x02\x53\x9d\xfa\x14\x7c\xa9\x27\x27\x78".into(),
+            &[b' '; 16].into(),
+        )
+        .decrypt_padded_vec_mut::<Pkcs7>(&secret[3..])?,
+    )?)
+}
+
+async fn get_secret_gnome_libsecret(
+    keyring2_path: std::path::PathBuf,
+) -> Result<String, Box<dyn std::error::Error>> {
     // "GnomeKeyring\n\r\x00\n\x00\x00": https://gitlab.gnome.org/GNOME/gnome-keyring/-/blob/main/docs/file-format.txt
     // "GnomeKeyring\n\r\x00\n\x01\x00": https://gitlab.gnome.org/GNOME/libsecret/-/blob/main/libsecret/secret-file-collection.c
 
@@ -25,7 +60,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         secret = item.secret().await?;
     }
 
-    let keyring2 = oo7::file::Keyring::load(args.keyring, secret).await?;
+    let keyring2 = oo7::file::Keyring::load(keyring2_path, secret).await?;
     let mut secret2 = "";
     let attributes = HashMap::from([("service", "element.io")]);
     for item in keyring2.search_items(&attributes).await? {
@@ -34,9 +69,34 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             secret2 = str::from_utf8(secret.as_bytes())?;
         }
     }
+    Ok(secret2.to_owned())
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::parse();
+
+    let config = serde_json::from_str::<serde_json::Value>(&std::fs::read_to_string(
+        args.element_dir.join("electron-config.json"),
+    )?)?;
+    // https://www.electronjs.org/docs/latest/api/safe-storage
+    let secret2 = match config["safeStorageBackend"].as_str().ok_or("")? {
+        "basic_text" => get_secret_basic_text(config).await,
+        "gnome_libsecret" => {
+            get_secret_gnome_libsecret(
+                args.keyring.unwrap_or(
+                    args.element_dir
+                        .join("../../.local/share/keyrings/default.keyring"),
+                ),
+            )
+            .await
+        }
+        _ => panic!("safeStorageBackend not supported"),
+    }?;
 
     let conn = rusqlite::Connection::open_with_flags(
-        args.events_db,
+        args.events_db
+            .unwrap_or(args.element_dir.join("EventStore/events.db")),
         rusqlite::OpenFlags::SQLITE_OPEN_READ_ONLY,
     )?;
     conn.pragma_update(None, "key", secret2)?;
